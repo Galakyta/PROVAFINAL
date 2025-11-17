35263ea5829a1c4bb9d34d68ffd404b06b56602c
Marcos / Galakyta <galakyta@gmail.com>
2025-11-17 11:44:57 -0300

    Add files via upload

---
+++ 20251116195645_AddImposto.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace prototipoGestao.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImposto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Imposto",
+                table: "Despesas",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Imposto",
+                table: "Despesas");
+        }
+    }
+}
